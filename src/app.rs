5fa5b9e3fff2019f705f68047578d49297d6d051
Hiron Das <hcdas.09@gmail.com>
2025-09-29 18:34:14 +0600

    icons are added on pages

--- src/app.rs
+++ src/app.rs
@@ -11,7 +11,7 @@ use crate::pages::HomePage;
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
         <!DOCTYPE html>
-        <html lang="en">
+        <html lang="en" class="h-full bg-gray-100">
             <head>
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
--- src/pages/login.rs
+++ src/pages/login.rs
@@ -1,12 +1,16 @@
 use leptos::prelude::*;
-
+use leptos_icons::Icon;
 /// Documentation for [`Login`]
 #[component]
 pub fn Login() -> impl IntoView {
     
 
     view! {
-        "Login Page"
+        <div class="flex justify-center items-center h-screen">
+        <div class="w-100 h-100 bg-white rounded-lg shadow-lg flex justify-center p-6">
+            "Login Page"
+            <Icon icon={icondata::AiUserOutlined} width="30" height="30"/>
+        </div>
+        </div>
     }
 }
-
